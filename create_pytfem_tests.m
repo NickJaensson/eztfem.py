6df734e213bcd6efd57203755ce68acb21969d52
Nick Jaensson <n.jaensson@gmail.com>
2023-08-10 21:42:18 +0200

    Add write_attrib function

--- create_pytfem_tests.m
+++ create_pytfem_tests.m
@@ -40,65 +40,20 @@ mywritelines("    self.assertTrue(np.allclose(sol_pytfem,sol_eztfem,atol=1e-15,r
 cmd = "quadrilateral2d([3,2],'quad9')";
 sol_eztfem = eval(cmd);
 
-
-
 mywritelines("  def test_quadrilaterial2d(self):");
 mywritelines("    sol_pytfem = "+cmd);
 
 mywritelines("    sol_eztfem = Mesh()");
+write_attrib("    ",sol_eztfem,"sol_eztfem")
 
-% fieldnames = fieldnames(sol_eztfem);
-% for k=1:numel(fieldnames)
-%     tmp = sol_eztfem.(fieldnames{k}));
-%     if isnumeric(tmp)
-%         if iscalar(tmp)
-%             mywritelines("    sol_eztfem.ndim = "+string(sol_eztfem.ndim));
-% 
-% 
-%         end
-%     end
-% end
-
-
-
-mywritelines("    sol_eztfem.ndim = "+string(sol_eztfem.ndim));
-mywritelines("    sol_eztfem.nnodes = "+string(sol_eztfem.nnodes));
-mywritelines("    sol_eztfem.elshape = "+string(sol_eztfem.elshape));
-mywritelines("    sol_eztfem.nelem = "+string(sol_eztfem.nelem));
-mywritelines("    sol_eztfem.elnumnod = "+string(sol_eztfem.elnumnod));
-mywritelines("    sol_eztfem.npoints = "+string(sol_eztfem.npoints));
-mywritelines("    sol_eztfem.ncurves = "+string(sol_eztfem.ncurves));
-write2Darr_i("    ",sol_eztfem.topology,"sol_eztfem.topology")
-write2Darr_r("    ",sol_eztfem.coor,"sol_eztfem.coor")
-write1Darr_i("    ",sol_eztfem.points,"sol_eztfem.points")
-
-
-
-
-%         mesh.curves[0]= Geometry(elshape=2,ndim=2,elnumnod=3,nnodes=3,nelem=1)
-%         mesh.curves[0].nodes = np.array([1,2,3],dtype=int)
-%         mesh.curves[0].topology = np.array([[[1,2,3],[1,2,3]])
-%  
-%         mesh.curves[1]= Geometry(elshape=2,ndim=2,elnumnod=3,nnodes=5,nelem=2)
-%         mesh.curves[1].nodes = np.array([3,6,9,12,15],dtype=int)
-% 
-%         mesh.curves[2]= Geometry(elshape=2,ndim=2,elnumnod=3,nnodes=3,nelem=1)
-%         mesh.curves[2].nodes = np.array([15,14,13],dtype=int)
-% 
-%         mesh.curves[3]= Geometry(elshape=2,ndim=2,elnumnod=3,nnodes=5,nelem=2)
-%         mesh.curves[3].nodes = np.array([13,10,7,4,1],dtype=int)
+for i=1:sol_eztfem.ncurves
+    mywritelines("    sol_eztfem.curves.append(Geometry())");
+    write_attrib("    ",sol_eztfem.curves(i),"sol_eztfem.curves["+string(i-1)+"]")
+end
 
 mywritelines("    # compensate for zero-based indexing");
 mywritelines("    sol_eztfem.topology = sol_eztfem.topology - 1");
 mywritelines("    sol_eztfem.points = sol_eztfem.points - 1");
-
-% for i=1:sol_eztfem.ncurves
-% 
-% 
-% 
-% end
-
-
 
 mywritelines("    self.assertTrue(sol_pytfem==sol_eztfem,'quadrilateral2d failed test!' )");
 
@@ -162,3 +117,33 @@ function write2Darr_i(skip,arr,name)
     mywritelines("    ],dtype=int)");
 
 end
+
+function write_attrib(skip,struct,name)
+
+    fns = fieldnames(struct);
+    for k=1:numel(fns)
+        tmp = struct.(fns{k});
+        if isnumeric(tmp)
+            if isscalar(tmp)
+                mywritelines(skip+name+"."+fns{k}+" = "+string(tmp));
+            elseif ndims(tmp) > 2
+                error("error write_attrib: dims("+fns{k}+") > 2")
+            end
+            if all(mod(tmp,1)==0,'all') % array of integers
+                if size(tmp,2)==1
+                    write1Darr_i(skip,tmp,name+"."+fns{k});
+                else
+                    write2Darr_i(skip,tmp,name+"."+fns{k});
+                end
+            else 
+                if size(tmp,2)==1
+                    write1Darr_r(skip,tmp,name+"."+fns{k}); 
+                else 
+                    write2Darr_r(skip,tmp,name+"."+fns{k});
+                end
+            end
+        end
+    end
+
+end
+
