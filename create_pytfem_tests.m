3c26e3cdcc815d79ef620df7fde63b3c711ec841
Nick Jaensson <n.jaensson@gmail.com>
2023-08-11 15:42:20 +0200

    Update create_pytfem_tests.m

--- create_pytfem_tests.m
+++ create_pytfem_tests.m
@@ -58,8 +58,10 @@ mywritelines("    self.assertTrue(np.allclose(grid_py,grid_ez," + ...
 %% run the problem in Matlab
 
 % define the commands to run in Matlab and python
-cmd_mesh_ez = "quadrilateral2d([1,2],'quad9')";
-cmd_mesh_py = "quadrilateral2d([1,2],'quad9')";
+cmd_mesh_ez = "quadrilateral2d([3,2],'quad9','origin',[1,1],'length',[4,3])";
+cmd_mesh_py = "quadrilateral2d([3,2],'quad9',origin=np.array([1,1]),length=np.array([4,3]))";
+% cmd_mesh_ez = "quadrilateral2d([8,12],'quad9','vertices',[1,1;2,2;3,3;4,4],'ratio',[2,3,2,3],'factor',[2,3,4,5])";
+% cmd_mesh_py = "quadrilateral2d([8,12],'quad9',vertices=np.array([[1,1],[2,2],[3,3],[4,4]]),ratio=np.array([2,3,2,3]),factor=np.array([2,3,4,5]))";
 
 % elementdof=[1,1,1,1,1,1,1,1,1;
 %             2,2,2,2,2,2,2,2,2]' ;
