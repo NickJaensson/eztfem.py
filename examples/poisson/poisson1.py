1fc9c3c5db728d890491602c892d0d7590414217
Nick Jaensson <n.jaensson@gmail.com>
2023-08-18 14:14:53 +0200

    Extended examples

--- examples/poisson/poisson1.py
+++ examples/poisson/poisson1.py
@@ -26,29 +26,74 @@ from scipy.sparse.linalg import spsolve
 
 import pretty_errors
 
-mesh = quadrilateral2d([3,2],'quad9')
+# create mesh
 
+print('mesh')
+mesh = quadrilateral2d([20,20],'quad9')
+
+# define the problem
+
+print('problem_definition')
 elementdof = np.array([[1,1,1,1,1,1,1,1,1],
                        [2,2,2,2,2,2,2,2,2]],dtype=int).transpose()
 problem = Problem(mesh,elementdof,nphysq=1)
 
+# define Gauss integration and basis functions
+
 user = User()
 shape='quad'
+
+print('gauss_legendre')
 user.xr, user.wg = gauss_legendre(shape,n=3)
+
+print('basis_function phi')
 user.phi, user.dphi = basis_function(shape,'Q2', user.xr )
+
+# user struct for setting problem coefficients, ...
 
 user.coorsys = 0
 user.alpha = 1
 user.funcnr = 4
 user.func = func
 
+# assemble the system matrix and vector
+
+print('build_system')
 A, f = build_system ( mesh, problem, poisson_elem, user )
 
+# define essential boundary conditions (Dirichlet)
+
+print('define_essential')
 iess = define_essential ( mesh, problem,'curves', [0,1,2,3] )
 
+# fill values for the essential boundary conditions
+
+print('fill_system_vector')
 uess = fill_system_vector ( mesh, problem, 'curves', [0,1], func, funcnr=3 )
 uess = fill_system_vector ( mesh, problem, 'curves', [2,3], func, funcnr=3, fin=uess )
 
+# apply essential boundary conditions to the system
+
+print('apply_essential')
 A, f, _ = apply_essential ( A, f, uess, iess )
 
+# solve the system 
+
+print('solve')
 u = spsolve(A.tocsr(), f)
+
+# compare with exact solution
+
+print('Difference with exact solution:')
+uex = fill_system_vector ( mesh, problem, 'nodes', np.arange(mesh.nnodes), func, funcnr=3 ) 
+
+print(max(abs(u-uex)))
+
+# gradient (dudx,dudy) of the solution 
+
+print('gradient (dudx,dudy)')
+xr = refcoor_nodal_points ( mesh )
+[user.phi,user.dphi]=basis_function('quad','Q2', xr )
+user.u = u
+
+#gradu = deriv_vector ( mesh, problem, poisson_deriv, user )
--- examples/stokes/stokes1.py
+++ examples/stokes/stokes1.py
@@ -28,12 +28,20 @@ from scipy.sparse.linalg import spsolve
 
 import pretty_errors
 
-mesh = quadrilateral2d([3,2],'quad9')
+# create mesh
 
+print('mesh')
+mesh = quadrilateral2d([20,20],'quad9')
+
+# define the problem
+
+print('problem_definition')
 elementdof = np.array([[2,2,2,2,2,2,2,2,2],
                        [1,0,1,0,1,0,1,0,0],
                        [1,1,1,1,1,1,1,1,1]],dtype=int).transpose()
 problem = Problem(mesh,elementdof,nphysq=2)
+
+# define Gauss integration and basis functions
 
 user = User()
 shape='quad'
@@ -41,30 +49,66 @@ user.xr, user.wg = gauss_legendre(shape,n=3)
 user.phi, user.dphi = basis_function(shape,'Q2', user.xr )
 user.psi, _ = basis_function(shape,'Q1', user.xr )
 
+# user struct for setting problem coefficients, ...
+
 user.coorsys = 0
 user.mu = 1
 user.funcnr = 0 
 user.func = func # not used when funcnr == 0
 
+# assemble the system matrix and vector
+
+print('build_system')
 A, f = build_system ( mesh, problem, stokes_elem, user )
 
+# define essential boundary conditions (Dirichlet)
+
+print('define_essential')
 iess = define_essential ( mesh, problem, 'curves',[0,1,2,3], degfd=0 )
 iess = define_essential ( mesh, problem, 'curves',[0,2,2,3], degfd=1, iessp=iess )
 iess = define_essential ( mesh, problem, 'points',[0], physq=1, iessp=iess )
 
+# fill values for the essential boundary conditions
+
+print('fill_system_vector')
 uess = fill_system_vector ( mesh, problem, 'curves', [0,1], func, funcnr=3 )
 uess = fill_system_vector ( mesh, problem, 'curves', [2,3], func, funcnr=3, fin=uess )
 
+# apply essential boundary conditions to the system
+
+print('apply_essential')
 A, f, _ = apply_essential ( A, f, uess, iess )
 
+# solve the system 
+
+print('solve')
 u = spsolve(A.tocsr(), f)
 
+# Pressure in all nodes for plotting
+
+print('pressure in nodes')
 xr = refcoor_nodal_points ( mesh )
 user.psi, _ = basis_function('quad','Q1', xr )
 user.u = u
 pressure = deriv_vector ( mesh, problem, stokes_pressure, user )
 
+# derivatives of the velocity
+
+print('velocity derivatives')
+xr = refcoor_nodal_points ( mesh )
 user.phi, user.dphi = basis_function('quad','Q2', xr )
+user.u = u
+
+user.comp = 0 # dudx
+dudx = deriv_vector ( mesh, problem, stokes_deriv, user ) 
+user.comp = 1 # dudy
+dudy = deriv_vector ( mesh, problem, stokes_deriv, user )
+user.comp = 2 # dvdx
+dvdx = deriv_vector ( mesh, problem, stokes_deriv, user ) 
+user.comp = 3 # dvdy
+dvdy = deriv_vector ( mesh, problem, stokes_deriv, user )
+user.comp = 4 # dvdx - dudy = vorticity
+omega = deriv_vector ( mesh, problem, stokes_deriv, user ) 
 user.comp = 6 # divu, divergence of the velocity field
 divu = deriv_vector ( mesh, problem, stokes_deriv, user ) 
 user.comp = 7 # gammadot, effective strain rate = sqrt(2II_D) 
