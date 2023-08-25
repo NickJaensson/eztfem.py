48d489acbc3e950daaf596a7690c27b863f28c18
Nick Jaensson <n.jaensson@gmail.com>
2023-08-25 23:38:16 +0200

    Update distribute_elements.py

--- src/distribute_elements.py
+++ src/distribute_elements.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-def distribute_elements(n, ratio, factor):
+def distribute_elements(nelem, ratio, factor):
     """
     DISTRIBUTE_ELEMENTS  Generate noequidistant n elements on the interval [0,1] 
       [ x ] = DISTRIBUTE_ELEMENTS ( n, ratio, factor )
       input:
-        n: number of elements
+        nelem: number of elements
         ratio, factor: 
           ratio = 0: equidistant mesh
           ratio = 1: the size of the last element is factor times the first
@@ -31,40 +31,36 @@ def distribute_elements(n, ratio, factor):
     if factor < 0:
         raise ValueError("Negative factor")
     
-    if n <= 1:
+    if nelem <= 1:
         raise ValueError("Number of elements must be at least 2")
 
     match ratio:
         case 0:
             g = 1
         case 1:
-            g = np.exp(np.log(factor) / (n - 1))
+            g = np.exp(np.log(factor) / (nelem - 1))
         case 2:
             g = factor
         case 3:
-            g = np.exp(-np.log(factor) / (n - 1))
+            g = np.exp(-np.log(factor) / (nelem - 1))
         case 4:
             g = 1 / factor
         case _:
             raise ValueError(f"Invalid value for ratio: {ratio}")
 
 #   generate mesh
-
-    fac = 1.0 + sum(g**i for i in range(1, n))
+    fac = 1.0 + sum(g**i for i in range(1, nelem))
 
 #   size of first element
-
     dx_1 = 1.0 / fac
 
 #   generate all elements 
-
-    dx_values = [dx_1 * g ** i for i in range(n)]
+    dx_values = [dx_1 * g ** i for i in range(nelem)]
     
-    x = np.zeros(n + 1)
+    x = np.zeros(nelem + 1)
     x[1:] = np.cumsum(np.array(dx_values))
 
 #   test whether x(n+1) = 1
-
     if abs(x[-1] - 1) > 1e-10:
         raise ValueError(f"End value x(n+1) != 1: {x[-1]:.5e}")
     else:
