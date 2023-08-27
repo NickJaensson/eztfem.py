7bae5260a223984da1c7ff2b6c1a52f726635521
Nick Jaensson <n.jaensson@gmail.com>
2023-08-27 12:56:11 +0200

    Start with de-vectorizing (for improved readibility)

--- src/quadrilateral2d.py
+++ src/quadrilateral2d.py
@@ -269,9 +269,8 @@ def rectangle2d_tria3(num_el, ratio, factor):
         curve.topology = np.zeros((elnumnod, nelem, 2), dtype=int)
 
         # local numbering
-        start_points = np.arange(0, nelem).reshape(nelem, 1)
-        local_numbering = start_points + np.arange(2)
-        curve.topology[:, :, 0] = local_numbering.T
+        for elem in range(nelem):
+            curve.topology[:, elem, 0] = np.arange(elem, elem + 2)
 
         # global numbering
         curve.topology[:, :, 1] = curve.nodes[ curve.topology[:, :, 0] ]
@@ -413,9 +412,8 @@ def rectangle2d_quad4(num_el, ratio, factor):
         curve.topology = np.zeros((elnumnod, nelem, 2), dtype=int)
 
         # local numbering
-        start_points = np.arange(0, nelem).reshape(nelem, 1)
-        local_numbering = start_points + np.arange(2)
-        curve.topology[:, :, 0] = local_numbering.T
+        for elem in range(nelem):
+            curve.topology[:, elem, 0] = np.arange(elem, elem + 2)
 
         # global numbering
         curve.topology[:, :, 1] = curve.nodes[ curve.topology[:, :, 0] ]
@@ -567,9 +565,8 @@ def rectangle2d_quad5(num_el, ratio, factor):
         curve.topology = np.zeros((elnumnod, nelem, 2), dtype=int)
 
         # local numbering
-        start_points = np.arange(0, nelem).reshape(nelem, 1)
-        local_numbering = start_points + np.arange(2)
-        curve.topology[:, :, 0] = local_numbering.T
+        for elem in range(nelem):
+            curve.topology[:, elem, 0] = np.arange(elem, elem + 2)
 
         # global numbering
         curve.topology[:, :, 1] = curve.nodes[ curve.topology[:, :, 0] ]
@@ -715,9 +712,8 @@ def rectangle2d_quad9(num_el, ratio, factor):
         curve.topology = np.zeros((elnumnod, nelem, 2), dtype=int)
 
         # local numbering
-        start_points = np.arange(0, 2*nelem, 2).reshape(nelem, 1)
-        local_numbering = start_points + np.arange(3)
-        curve.topology[:, :, 0] = local_numbering.T
+        for elem in range(nelem):
+            curve.topology[:, elem, 0] = np.arange(2 * elem, 2 * elem + 3)
 
         # global numbering
         curve.topology[:, :, 1] = curve.nodes[ curve.topology[:, :, 0] ]
