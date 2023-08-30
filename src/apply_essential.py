f8052c1394590a25514e314910994a693517b323
Nick Jaensson <n.jaensson@gmail.com>
2023-08-30 14:54:26 +0200

    Update apply_essential.py

--- src/apply_essential.py
+++ src/apply_essential.py
@@ -3,20 +3,19 @@ from scipy.sparse import eye, lil_matrix
 
 def apply_essential(Ain, fin, uess, iess):
     """
-    Apply essential degrees of freedom.
-
-    Parameters:
+    APPLY_ESSENTIAL  apply essential degrees of freedom
+      [ A, f, Aup ] = APPLY_ESSENTIAL ( Ain, fin, iess )
+      input:
         Ain: system matrix
-        fin: right-hand side
+        fin: right-hand side.
         uess: a system vector containing the values for essential bc
         iess: index of defined essential degrees
-
-    Returns:
-        A, f: the system matrix and right-hand side with essential bc applied
+      output:
+        A, f: the system matrix and right-hand side with ess bc applied
         Aup: matrix partition of unknown rows and prescribed columns.
     """
     
-    # Copy the system matrix and vector
+    # copy the system matrix and vector
     A = Ain.copy()
     f = np.array(fin)
     
@@ -24,21 +23,20 @@ def apply_essential(Ain, fin, uess, iess):
     npp = iess.shape[0]
     nu = nd - npp
 
-    # Find iu
+    # find iu
     tmp = np.zeros(nd, dtype=int)
     tmp[iess] = 1
     iu = np.where(tmp == 0)[0]
 
-    # Extract Aup
-
+    # extract Aup
     Aup = Ain[iu,:][:,iess]
 
-    # Modify A
+    # modify A
     A[iu[:,None], iess] = lil_matrix((nu, npp))
     A[iess[:,None], iu] = lil_matrix((npp, nu))
     A[iess[:,None], iess] = eye(npp)
 
-    # Modify f
+    # modify f
     f[iu] -= Aup.dot(uess[iess])
     f[iess] = uess[iess]
 
