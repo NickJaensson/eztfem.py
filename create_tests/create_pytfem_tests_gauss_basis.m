cec75041ecd1fc21f3a95385188bfe04f8b649a1
Nick Jaensson <n.jaensson@gmail.com>
2023-08-30 22:32:21 +0200

    Update create_pytfem_tests_gauss_basis.m

--- create_tests/create_pytfem_tests_gauss_basis.m
+++ create_tests/create_pytfem_tests_gauss_basis.m
@@ -12,21 +12,6 @@ addpath(append(eztfempath,"addons/poisson"))
 addpath(append(eztfempath,"examples/poisson"))  % use func.m from poisson
 
 %% Matlab file to generate testing code using eztfem for pytfem 
-% NOTE: arrays are written "as is" (looping of the first index, then the
-% second etc.). It is assumed that in Python they are also read like this
-% 
-% NOTE2: Python has real 1D arrays, whereas Matlab 1D arrays are N x 1
-% (i.e., ndims([1,2]) gives as output 2)
-% This can lead to problems when comparing arrays, since these are not
-% equivalent. 
-% 
-% This is handled on the Matlab side by:
-%  - write scalars as scalars (using isscalar)
-%  - write 2D arrays (so can be 1D or "true" 2D) as 1D when second dim == 1
-%  - write 3D arrays always as a full 3D array
-%
-% And on the Python side:
-%  - in Python: use numpy.squeeze on all 2D arrays to get rid of dim == 1
 
 
 %% filename for python test file
@@ -47,8 +32,9 @@ mywritelines("class TestPytfem(unittest.TestCase):");
 
 % write s number of tests for Guass and basis functions
 
-tests = {'quad',2,'Q2';
-         'quad',3,'Q2'};
+tests = {'line',1,'P0';
+         'line',2,'P1';
+         'line',3,'P2'};
 
 for ii = 1:size(tests,1)
 
@@ -71,10 +57,14 @@ for ii = 1:size(tests,1)
     %% test for gauss_legendre
     
     mywritelines("  def test_gauss_legendre"+string(ii)+"(self):");
-    write2Darr_r("     ",xr_ez,"xr_ez");
+    if ( size(xr_ez,1) == 1 || size(xr_ez,2) == 1 )
+        write1Darr_r("     ",xr_ez,"xr_ez");
+    else
+        write2Darr_r("     ",xr_ez,"xr_ez");
+    end
     write1Darr_r("     ",wg_ez,"wg_ez");
     mywritelines(cmd_gauss_py);
-    mywritelines("    check1=np.allclose(xr_py,xr_ez,atol=1e-15,rtol=0)")
+    mywritelines("    check1=np.allclose(np.squeeze(xr_py),np.squeeze(xr_ez),atol=1e-15,rtol=0)")
     mywritelines("    check2=np.allclose(wg_py,wg_ez,atol=1e-15,rtol=0)")
     mywritelines("    self.assertTrue(check1 and check2,'gauss_legendre failed test!' )");
     
@@ -82,12 +72,16 @@ for ii = 1:size(tests,1)
     %% test for basis_function
     
     mywritelines("  def test_basis_function"+string(ii)+"(self):");
-    write2Darr_r("     ",phi_ez,"phi_ez");
-    write3Darr_r("     ",dphi_ez,"dphi_ez");
+    if ( size(phi_ez,1) == 1 || size(phi_ez,2) == 1 )
+        write1Darr_r("     ",phi_ez,"phi_ez");
+    else
+        write2Darr_r("     ",phi_ez,"phi_ez");
+    end
+    write3Darr_r("     ",squeeze(dphi_ez),"dphi_ez");
     mywritelines(cmd_gauss_py);
     mywritelines(cmd_basis_py);
-    mywritelines("    check1=np.allclose(phi_py,phi_ez,atol=1e-15,rtol=0)")
-    mywritelines("    check2=np.allclose(dphi_py,dphi_ez,atol=1e-15,rtol=0)")
+    mywritelines("    check1=np.allclose(np.squeeze(phi_py),np.squeeze(phi_ez),atol=1e-15,rtol=0)")
+    mywritelines("    check2=np.allclose(np.squeeze(dphi_py),np.squeeze(dphi_ez),atol=1e-15,rtol=0)")
     mywritelines("    self.assertTrue(check1 and check2,'basis_functions failed test!' )");
 
 end
