25e6e5993a7bf67410697fd8615b56a8974b0e60
Nick Jaensson <n.jaensson@gmail.com>
2023-08-30 14:31:14 +0200

    Split tests into poisson and stokes

--- create_pytfem_tests_stokes.m
+++ create_pytfem_tests_stokes.m
@@ -5,7 +5,6 @@ addpath(eztfempath);
 addpath(append(eztfempath,"src"))
 addpath(append(eztfempath,"addons/plotlib"))
 addpath(append(eztfempath,"addons/meshes"))
-addpath(append(eztfempath,"addons/poisson"))
 addpath(append(eztfempath,"addons/stokes"))
 
 addpath(append(eztfempath,"examples/poisson"))  % use func.m from poisson
@@ -30,160 +29,92 @@ addpath(append(eztfempath,"examples/poisson"))  % use func.m from poisson
 
 %% filename for python test file
 global fn 
-fn = "~/Desktop/eztfem.py/dotest.py"; 
+fn = "~/Desktop/eztfem.py/dotest_stokes.py"; 
 
 
 %% run the problem in eztfem
 
 problemtype = "stokes";
 
-% mesh_ez = quadrilateral2d([1,2],'quad9','origin',[1,1],'length',[4,3]);
-mesh_ez = quadrilateral2d([3,2],'quad9','vertices',[1,1;2,2;2,4;1,4],'ratio',[1,2,3,4],'factor',[1.2,1.3,1.4,1.5]);
+mesh_ez = quadrilateral2d([3,2],'quad9','origin',[1,1],'length',[4,3]);
 
-if problemtype == "poisson"
+elementdof_ez = [2,2,2,2,2,2,2,2,2;
+                 1,0,1,0,1,0,1,0,0;
+                 1,1,1,1,1,1,1,1,1]' ;
+problem_ez = problem_definition(mesh_ez,elementdof_ez,'nphysq',2);
+[user_ez.xr,user_ez.wg] = gauss_legendre('quad','n', 3 );
+[user_ez.phi,user_ez.dphi] = basis_function('quad','Q2', user_ez.xr );
+[user_ez.psi,~] = basis_function('quad','Q1', user_ez.xr );
+user_ez.coorsys = 0 ;
+user_ez.mu = 1 ;
+user_ez.funcnr = 0 ;
+user_ez.func = @func ;
+[A_ez,f_ez] = build_system ( mesh_ez, problem_ez, @stokes_elem, user_ez);
+iess_ez = define_essential ( mesh_ez, problem_ez, 'curves', [1 2 3 4], 'degfd', 1 ) ;
+iess_ez = define_essential ( mesh_ez, problem_ez, 'curves', [1 2 3 4], 'degfd', 2, 'iessp', iess_ez ) ;
+iess_ez = define_essential ( mesh_ez, problem_ez, 'points', 1, 'physq', 2, 'iessp', iess_ez ) ;
 
-    elementdof_ez = [1,1,1,1,1,1,1,1,1; 2,2,2,2,2,2,2,2,2]' ;
-    problem_ez = problem_definition(mesh_ez,elementdof_ez,'nphysq',1);
-    [user_ez.xr,user_ez.wg] = gauss_legendre('quad','n', 3 );
-    [user_ez.phi,user_ez.dphi] = basis_function('quad','Q2', user_ez.xr );
-    user_ez.coorsys = 0 ;
-    user_ez.alpha = 1 ;
-    user_ez.funcnr = 4 ;
-    user_ez.func = @func ;
-    [A_ez,f_ez] = build_system ( mesh_ez, problem_ez, @poisson_elem, user_ez);
-    iess_ez = define_essential ( mesh_ez, problem_ez, 'curves', [1 2 3 4], 'degfd', 1 ) ;
+uess_ez = fill_system_vector ( mesh_ez, problem_ez, 'curves', [1,2], @func, 'funcnr', 3 );
+uess_ez = fill_system_vector ( mesh_ez, problem_ez, 'curves', [3,4], @func, 'funcnr', 3, 'fin', uess_ez );
 
-    uess_ez = fill_system_vector ( mesh_ez, problem_ez, 'curves', [1,2], @func, 'funcnr', 3 );
-    uess_ez = fill_system_vector ( mesh_ez, problem_ez, 'curves', [3,4], @func, 'funcnr', 3, 'fin', uess_ez );
+[A_ez2, f_ez2] = apply_essential ( A_ez, f_ez, uess_ez, iess_ez );
 
-    [A_ez2, f_ez2] = apply_essential ( A_ez, f_ez, uess_ez, iess_ez );
-    
-    u_ez = A_ez2\f_ez2 ;
+u_ez = A_ez2\f_ez2 ;
 
-    user_ez2 = user_ez;
-    xr_ez = refcoor_nodal_points ( mesh_ez ) ;
-    [user_ez2.phi,user_ez2.dphi] = basis_function('quad','Q2', xr_ez ) ;
-    user_ez2.u = u_ez ;
-    gradu_ez = deriv_vector ( mesh_ez, problem_ez, @poisson_deriv, user_ez2 ) ;
+user_ez2 = user_ez;
+xr_ez = refcoor_nodal_points ( mesh_ez ) ;
+[user_ez2.psi] = basis_function('quad','Q1', xr_ez ) ;
+user_ez2.u = u_ez ;
+pressure_ez = deriv_vector ( mesh_ez, problem_ez, @stokes_pressure, user_ez2 ) ;
 
-elseif problemtype == "stokes"
-
-    elementdof_ez = [2,2,2,2,2,2,2,2,2;
-                     1,0,1,0,1,0,1,0,0;
-                     1,1,1,1,1,1,1,1,1]' ;
-    problem_ez = problem_definition(mesh_ez,elementdof_ez,'nphysq',2);
-    [user_ez.xr,user_ez.wg] = gauss_legendre('quad','n', 3 );
-    [user_ez.phi,user_ez.dphi] = basis_function('quad','Q2', user_ez.xr );
-    [user_ez.psi,~] = basis_function('quad','Q1', user_ez.xr );
-    user_ez.coorsys = 0 ;
-    user_ez.mu = 1 ;
-    user_ez.funcnr = 0 ;
-    user_ez.func = @func ;
-    [A_ez,f_ez] = build_system ( mesh_ez, problem_ez, @stokes_elem, user_ez);
-    iess_ez = define_essential ( mesh_ez, problem_ez, 'curves', [1 2 3 4], 'degfd', 1 ) ;
-    iess_ez = define_essential ( mesh_ez, problem_ez, 'curves', [1 2 3 4], 'degfd', 2, 'iessp', iess_ez ) ;
-    iess_ez = define_essential ( mesh_ez, problem_ez, 'points', 1, 'physq', 2, 'iessp', iess_ez ) ;
-
-    uess_ez = fill_system_vector ( mesh_ez, problem_ez, 'curves', [1,2], @func, 'funcnr', 3 );
-    uess_ez = fill_system_vector ( mesh_ez, problem_ez, 'curves', [3,4], @func, 'funcnr', 3, 'fin', uess_ez );
-
-    [A_ez2, f_ez2] = apply_essential ( A_ez, f_ez, uess_ez, iess_ez );
-
-    u_ez = A_ez2\f_ez2 ;
-
-    user_ez2 = user_ez;
-    xr_ez = refcoor_nodal_points ( mesh_ez ) ;
-    [user_ez2.psi] = basis_function('quad','Q1', xr_ez ) ;
-    user_ez2.u = u_ez ;
-    pressure_ez = deriv_vector ( mesh_ez, problem_ez, @stokes_pressure, user_ez2 ) ;
-    
-    [user_ez2.phi,user_ez2.dphi]=basis_function('quad','Q2', xr_ez ) ;
-    user_ez2.comp = 7 ; % divu, divergence of the velocity field
-    divu_ez = deriv_vector ( mesh_ez, problem_ez, @stokes_deriv, user_ez2 ) ;
-    user_ez2.comp = 8 ; % gammadot, effective strain rate = sqrt(2II_D) 
-    gammadot_ez = deriv_vector ( mesh_ez, problem_ez, @stokes_deriv, user_ez2 ) ;
-
-else
-
-    error('Wrong problemtype: '+problemtype);
-
-end
+[user_ez2.phi,user_ez2.dphi]=basis_function('quad','Q2', xr_ez ) ;
+user_ez2.comp = 7 ; % divu, divergence of the velocity field
+divu_ez = deriv_vector ( mesh_ez, problem_ez, @stokes_deriv, user_ez2 ) ;
+user_ez2.comp = 8 ; % gammadot, effective strain rate = sqrt(2II_D) 
+gammadot_ez = deriv_vector ( mesh_ez, problem_ez, @stokes_deriv, user_ez2 ) ;
 
 
 %% define the same commands for pytfem
 
-% cmd_mesh_py =       "    mesh_py = quadrilateral2d([1,2],'quad9',origin=np.array([1,1]),length=np.array([4,3]))";
-cmd_mesh_py =       "    mesh_py = quadrilateral2d([3,2],'quad9',vertices=np.array([[1,1],[2,2],[2,4],[1,4]]),ratio=np.array([1,2,3,4]),factor=np.array([1.2,1.3,1.4,1.5]))";
+cmd_mesh_py =       "    mesh_py = quadrilateral2d([3,2],'quad9',origin=np.array([1,1]),length=np.array([4,3]))";
 
-if problemtype == "poisson"
+cmd_elementdof_py = "    elementdof_py = np.array([[2,2,2,2,2,2,2,2,2],[1,0,1,0,1,0,1,0,0],[1,1,1,1,1,1,1,1,1]]).T";
+cmd_problem_py =    "    problem_py = Problem(mesh_py,elementdof_py,nphysq=2)";
+cmd_fill_user_py =  "    user_py = User();" + ...
+                    "    user_py.coorsys = 0;"+...
+                    "    user_py.mu = 1;"+...
+                    "    user_py.funcnr = 0; "+...
+                    "    user_py.func = func";
+cmd_gauss_py =      "    user_py.xr, user_py.wg = gauss_legendre('quad',n=3 )";
+cmd_basis_py =      "    user_py.phi, user_py.dphi = basis_function('quad','Q2', user_py.xr );"+...
+                    "    user_py.psi, _ = basis_function('quad','Q1', user_py.xr )";
 
-    cmd_elementdof_py = "    elementdof_py = np.array([[1,1,1,1,1,1,1,1,1],[2,2,2,2,2,2,2,2,2]]).T"; 
-    cmd_problem_py =    "    problem_py = Problem(mesh_py,elementdof_py,nphysq=1)";
-    cmd_fill_user_py =  "    user_py = User();" + ...
-                        "    user_py.coorsys = 0;"+...
-                        "    user_py.alpha = 1;"+...
-                        "    user_py.funcnr = 4; "+...
-                        "    user_py.func = func";
-    cmd_gauss_py =      "    user_py.xr, user_py.wg = gauss_legendre('quad',n=3 )";
-    cmd_basis_py =      "    user_py.phi, user_py.dphi = basis_function('quad','Q2', user_py.xr );";
+cmd_build_sys_py =  "    A_py,f_py = build_system ( mesh_py, problem_py, stokes_elem, user_py)";
+cmd_define_ess_py = "    iess_py = define_essential ( mesh_py, problem_py,'curves', [0,1,2,3], degfd=0 );"+...
+                    "    iess_py = define_essential ( mesh_py, problem_py,'curves', [0,1,2,3], degfd=1, iessp=iess_py );"+...
+                    "    iess_py = define_essential ( mesh_py, problem_py,'points', 0, physq=1, iessp=iess_py  )";
 
-    cmd_build_sys_py =  "    A_py,f_py = build_system ( mesh_py, problem_py, poisson_elem, user_py)";
-    cmd_define_ess_py = "    iess_py = define_essential ( mesh_py, problem_py,'curves', [0,1,2,3], degfd=0 );";
+cmd_fill_sys_py =   "    uess_py = fill_system_vector ( mesh_py, problem_py, 'curves', [0,1], func, funcnr=3 );"+...
+                    "    uess_py = fill_system_vector ( mesh_py, problem_py, 'curves', [2,3], func, funcnr=3, fin=uess_py )";
 
-    cmd_fill_sys_py =   "    uess_py = fill_system_vector ( mesh_py, problem_py, 'curves', [0,1], func, funcnr=3 );"+...
-                        "    uess_py = fill_system_vector ( mesh_py, problem_py, 'curves', [2,3], func, funcnr=3, fin=uess_py )";
+cmd_apply_ess_py =  "    A_py2, f_py2, _ = apply_essential ( A_py, f_py, uess_py, iess_py )";
 
-    cmd_apply_ess_py =  "    A_py2, f_py2, _ = apply_essential ( A_py, f_py, uess_py, iess_py )";
+cmd_solve_py     =  "    u_py = spsolve(A_py2.tocsr(), f_py2)";
 
-    cmd_solve_py     =  "    u_py = spsolve(A_py2.tocsr(), f_py2)";
+cmd_deriv_vector =  "    user_py2 = user_py;" + ...
+                    "    xr_py = refcoor_nodal_points ( mesh_py );"+...
+                    "    user_py2.psi, _ = basis_function('quad','Q1', xr_py );"+...
+                    "    user_py2.u = u_py;"+...
+                    "    pressure_py = deriv_vector ( mesh_py, problem_py, stokes_pressure, user_py2 )";
 
-    cmd_deriv_vector =  "    user_py2 = user_py;" + ...
-                        "    xr_py = refcoor_nodal_points ( mesh_py );"+...
-                        "    user_py2.phi, user_py2.dphi = basis_function('quad','Q2', xr_py );"+...
-                        "    user_py2.u = u_py;"+...
-                        "    gradu_py = deriv_vector ( mesh_py, problem_py, poisson_deriv, user_py2 )";
-
-elseif problemtype == "stokes"
-
-    cmd_elementdof_py = "    elementdof_py = np.array([[2,2,2,2,2,2,2,2,2],[1,0,1,0,1,0,1,0,0],[1,1,1,1,1,1,1,1,1]]).T";
-    cmd_problem_py =    "    problem_py = Problem(mesh_py,elementdof_py,nphysq=2)";
-    cmd_fill_user_py =  "    user_py = User();" + ...
-                        "    user_py.coorsys = 0;"+...
-                        "    user_py.mu = 1;"+...
-                        "    user_py.funcnr = 0; "+...
-                        "    user_py.func = func";
-    cmd_gauss_py =      "    user_py.xr, user_py.wg = gauss_legendre('quad',n=3 )";
-    cmd_basis_py =      "    user_py.phi, user_py.dphi = basis_function('quad','Q2', user_py.xr );"+...
-                        "    user_py.psi, _ = basis_function('quad','Q1', user_py.xr )";
-
-    cmd_build_sys_py =  "    A_py,f_py = build_system ( mesh_py, problem_py, stokes_elem, user_py)";
-    cmd_define_ess_py = "    iess_py = define_essential ( mesh_py, problem_py,'curves', [0,1,2,3], degfd=0 );"+...
-                        "    iess_py = define_essential ( mesh_py, problem_py,'curves', [0,1,2,3], degfd=1, iessp=iess_py );"+...
-                        "    iess_py = define_essential ( mesh_py, problem_py,'points', 0, physq=1, iessp=iess_py  )";
-
-    cmd_fill_sys_py =   "    uess_py = fill_system_vector ( mesh_py, problem_py, 'curves', [0,1], func, funcnr=3 );"+...
-                        "    uess_py = fill_system_vector ( mesh_py, problem_py, 'curves', [2,3], func, funcnr=3, fin=uess_py )";
-
-    cmd_apply_ess_py =  "    A_py2, f_py2, _ = apply_essential ( A_py, f_py, uess_py, iess_py )";
-
-    cmd_solve_py     =  "    u_py = spsolve(A_py2.tocsr(), f_py2)";
-
-    cmd_deriv_vector =  "    user_py2 = user_py;" + ...
-                        "    xr_py = refcoor_nodal_points ( mesh_py );"+...
-                        "    user_py2.psi, _ = basis_function('quad','Q1', xr_py );"+...
-                        "    user_py2.u = u_py;"+...
-                        "    pressure_py = deriv_vector ( mesh_py, problem_py, stokes_pressure, user_py2 )";
-
-    cmd_deriv_vector2 =  "    user_py2 = user_py;" + ...
-                        "    xr_py = refcoor_nodal_points ( mesh_py );"+... 
-                        "    user_py2.phi, user_py2.dphi = basis_function('quad','Q2', xr_py );"+...
-                        "    user_py2.u = u_py;"+...
-                        "    user_py2.comp = 6;"+...
-                        "    divu_py = deriv_vector ( mesh_py, problem_py, stokes_deriv, user_py2 );"+...
-                        "    user_py2.comp = 7;"+...
-                        "    gammadot_py = deriv_vector ( mesh_py, problem_py, stokes_deriv, user_py2 )";
-
-end
+cmd_deriv_vector2 =  "    user_py2 = user_py;" + ...
+                    "    xr_py = refcoor_nodal_points ( mesh_py );"+... 
+                    "    user_py2.phi, user_py2.dphi = basis_function('quad','Q2', xr_py );"+...
+                    "    user_py2.u = u_py;"+...
+                    "    user_py2.comp = 6;"+...
+                    "    divu_py = deriv_vector ( mesh_py, problem_py, stokes_deriv, user_py2 );"+...
+                    "    user_py2.comp = 7;"+...
+                    "    gammadot_py = deriv_vector ( mesh_py, problem_py, stokes_deriv, user_py2 )";
 
 
 %% write some header stuff
@@ -199,11 +130,9 @@ mywritelines("from src.user_class import User");
 mywritelines("from src.gauss_legendre import gauss_legendre");
 mywritelines("from src.basis_function import basis_function");
 mywritelines("from src.build_system import build_system");
-mywritelines("from addons.poisson.poisson_elem import poisson_elem");
 mywritelines("from addons.stokes.stokes_elem import stokes_elem");
 mywritelines("from addons.stokes.stokes_pressure import stokes_pressure");
 mywritelines("from addons.stokes.stokes_deriv import stokes_deriv");
-mywritelines("from addons.poisson.poisson_deriv import poisson_deriv");
 
 mywritelines("from src.define_essential import define_essential");
 
@@ -375,74 +304,49 @@ mywritelines("    self.assertTrue(np.allclose(u_py,u_ez,atol=1e-12,rtol=0)," + .
 
 %% tests for deriv_vector
 
-if problemtype == 'poisson'
+mywritelines("  def test_deriv_vector(self):");
+mywritelines("    pressure_ez = Vector()");
+write_attrib("    ",pressure_ez,"pressure_ez")
+mywritelines("    pressure_ez.vec += -1 # compensate for Python indexing"); 
 
-    mywritelines("  def test_deriv_vector(self):");
-    mywritelines("    gradu_ez = Vector()");
-    write_attrib("    ",gradu_ez,"gradu_ez")
-    mywritelines("    gradu_ez.vec += -1 # compensate for Python indexing"); 
-    
-    mywritelines(cmd_mesh_py);
-    mywritelines(cmd_elementdof_py);
-    mywritelines(cmd_problem_py);
-    mywritelines(cmd_fill_user_py);
-    mywritelines(cmd_gauss_py);
-    mywritelines(cmd_basis_py);
-    mywritelines(cmd_build_sys_py);
-    mywritelines(cmd_define_ess_py);
-    mywritelines(cmd_fill_sys_py);
-    mywritelines(cmd_apply_ess_py);
-    mywritelines(cmd_solve_py);
-    mywritelines(cmd_deriv_vector);
-    mywritelines("    self.assertTrue(gradu_ez==gradu_py," + ...
-        "'deriv_vector failed test, max diff = '+str((abs(gradu_ez.u-gradu_py.u)).max()) )");
+mywritelines(cmd_mesh_py);
+mywritelines(cmd_elementdof_py);
+mywritelines(cmd_problem_py);
+mywritelines(cmd_fill_user_py);
+mywritelines(cmd_gauss_py);
+mywritelines(cmd_basis_py);
+mywritelines(cmd_build_sys_py);
+mywritelines(cmd_define_ess_py);
+mywritelines(cmd_fill_sys_py);
+mywritelines(cmd_apply_ess_py);
+mywritelines(cmd_solve_py);
+mywritelines(cmd_deriv_vector);
+mywritelines("    self.assertTrue(pressure_ez==pressure_py," + ...
+    "'deriv_vector failed test, max diff = '+str((abs(pressure_ez.u-pressure_py.u)).max()) )");
 
-elseif problemtype == 'stokes'
+mywritelines("  def test_deriv_vector2(self):");
+mywritelines("    divu_ez = Vector()");
+write_attrib("    ",divu_ez,"divu_ez")
+mywritelines("    divu_ez.vec += -1 # compensate for Python indexing");
+mywritelines("    gammadot_ez = Vector()");
+write_attrib("    ",gammadot_ez,"gammadot_ez")
+mywritelines("    gammadot_ez.vec += -1 # compensate for Python indexing"); 
 
-    mywritelines("  def test_deriv_vector(self):");
-    mywritelines("    pressure_ez = Vector()");
-    write_attrib("    ",pressure_ez,"pressure_ez")
-    mywritelines("    pressure_ez.vec += -1 # compensate for Python indexing"); 
-    
-    mywritelines(cmd_mesh_py);
-    mywritelines(cmd_elementdof_py);
-    mywritelines(cmd_problem_py);
-    mywritelines(cmd_fill_user_py);
-    mywritelines(cmd_gauss_py);
-    mywritelines(cmd_basis_py);
-    mywritelines(cmd_build_sys_py);
-    mywritelines(cmd_define_ess_py);
-    mywritelines(cmd_fill_sys_py);
-    mywritelines(cmd_apply_ess_py);
-    mywritelines(cmd_solve_py);
-    mywritelines(cmd_deriv_vector);
-    mywritelines("    self.assertTrue(pressure_ez==pressure_py," + ...
-        "'deriv_vector failed test, max diff = '+str((abs(pressure_ez.u-pressure_py.u)).max()) )");
-
-    mywritelines("  def test_deriv_vector2(self):");
-    mywritelines("    divu_ez = Vector()");
-    write_attrib("    ",divu_ez,"divu_ez")
-    mywritelines("    divu_ez.vec += -1 # compensate for Python indexing");
-    mywritelines("    gammadot_ez = Vector()");
-    write_attrib("    ",gammadot_ez,"gammadot_ez")
-    mywritelines("    gammadot_ez.vec += -1 # compensate for Python indexing"); 
-    
-    mywritelines(cmd_mesh_py);
-    mywritelines(cmd_elementdof_py);
-    mywritelines(cmd_problem_py);
-    mywritelines(cmd_fill_user_py);
-    mywritelines(cmd_gauss_py);
-    mywritelines(cmd_basis_py);
-    mywritelines(cmd_build_sys_py);
-    mywritelines(cmd_define_ess_py);
-    mywritelines(cmd_fill_sys_py);
-    mywritelines(cmd_apply_ess_py);
-    mywritelines(cmd_solve_py);
-    mywritelines(cmd_deriv_vector2);
-    mywritelines("    self.assertTrue(divu_ez==divu_py and gammadot_ez==gammadot_py,"+...
-        "'deriv_vector2 failed test, max diff = '+str((abs(divu_ez.u-divu_py.u)).max())"+...
-        "+' and '+str((abs(gammadot_ez.u-gammadot_py.u)).max()))");
-end
+mywritelines(cmd_mesh_py);
+mywritelines(cmd_elementdof_py);
+mywritelines(cmd_problem_py);
+mywritelines(cmd_fill_user_py);
+mywritelines(cmd_gauss_py);
+mywritelines(cmd_basis_py);
+mywritelines(cmd_build_sys_py);
+mywritelines(cmd_define_ess_py);
+mywritelines(cmd_fill_sys_py);
+mywritelines(cmd_apply_ess_py);
+mywritelines(cmd_solve_py);
+mywritelines(cmd_deriv_vector2);
+mywritelines("    self.assertTrue(divu_ez==divu_py and gammadot_ez==gammadot_py,"+...
+    "'deriv_vector2 failed test, max diff = '+str((abs(divu_ez.u-divu_py.u)).max())"+...
+    "+' and '+str((abs(gammadot_ez.u-gammadot_py.u)).max()))");
 
 
 %% helper functions
