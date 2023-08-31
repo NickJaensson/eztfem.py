dd4d1a576f65fe645d9c1aad5d45ffee6c753879
Nick Jaensson <n.jaensson@gmail.com>
2023-08-31 11:40:29 +0200

    Implement all remaining shape functions

--- src/basis_function.py
+++ src/basis_function.py
@@ -78,16 +78,114 @@ def basis_triangle_P0(xr):
     return phi, dphi
 
 def basis_triangle_P1(xr):
-    raise NotImplementedError
+  phi, dphi = barycentric ( xr )
+  return phi, dphi
 
 def basis_triangle_P1plus(xr):
-    raise NotImplementedError
+    """
+    Calculate the phi and dphi based on the barycentric coordinates.
+    
+    Parameters:
+    - xr: Reference coordinates.
+    - barycentric: A function to calculate barycentric coordinates and their derivatives.
+
+    Returns:
+    - phi: The computed phi matrix.
+    - dphi: Derivative of phi.
+    """
+    
+    ni, _ = xr.shape
+    nn = 4
+    phi = np.zeros((ni, nn))
+    dphi = np.zeros((ni, nn, 2))
+    dbubble = np.zeros((ni, 1, 2))
+
+    lam, dlam = barycentric(xr)
+
+    bubble = lam[:, 0] * lam[:, 1] * lam[:, 2]
+    
+    phi[:, 0] = lam[:, 0] - 9 * bubble
+    phi[:, 1] = lam[:, 1] - 9 * bubble
+    phi[:, 2] = lam[:, 2] - 9 * bubble
+    phi[:, 3] = 27 * bubble
+
+    for dm in range(2):
+        dbubble[:, 0, dm] = (dlam[:, 0, dm] * lam[:, 1] * lam[:, 2] + 
+                             lam[:, 0] * dlam[:, 1, dm] * lam[:, 2] + 
+                             lam[:, 0] * lam[:, 1] * dlam[:, 2, dm])
+
+    dphi[:, 0, :] = dlam[:, 0, :] - 9 * dbubble[:, 0, :]
+    dphi[:, 1, :] = dlam[:, 1, :] - 9 * dbubble[:, 0, :]
+    dphi[:, 2, :] = dlam[:, 2, :] - 9 * dbubble[:, 0, :]
+    dphi[:, 3, :] = 27 * dbubble[:, 0, :]
+
+    return phi, dphi
+
+# Assuming you have defined the `barycentric` function earlier.
+# xr = np.array(...)  # You will need to provide some sample xr matrix here.
+# phi, dphi = compute_phi(xr, barycentric)
+# print(phi)
+# print(dphi)
 
 def basis_triangle_P2(xr):
-    raise NotImplementedError
+    ni, _ = xr.shape
+    nn = 6
+    phi = np.zeros((ni, nn))
+    dphi = np.zeros((ni, nn, 2))
+
+    lam, dlam = barycentric(xr)
+
+    phi[:, 0] = lam[:, 0] * (2 * lam[:, 0] - 1)
+    phi[:, 1] = 4 * lam[:, 0] * lam[:, 1]
+    phi[:, 2] = lam[:, 1] * (2 * lam[:, 1] - 1)
+    phi[:, 3] = 4 * lam[:, 1] * lam[:, 2]
+    phi[:, 4] = lam[:, 2] * (2 * lam[:, 2] - 1)
+    phi[:, 5] = 4 * lam[:, 2] * lam[:, 0]
+
+    for dm in range(2):
+        dphi[:, 0, dm] = (4 * lam[:, 0] - 1) * dlam[:, 0, dm]
+        dphi[:, 1, dm] = 4 * (dlam[:, 0, dm] * lam[:, 1] + lam[:, 0] * dlam[:, 1, dm])
+        dphi[:, 2, dm] = (4 * lam[:, 1] - 1) * dlam[:, 1, dm]
+        dphi[:, 3, dm] = 4 * (dlam[:, 1, dm] * lam[:, 2] + lam[:, 1] * dlam[:, 2, dm])
+        dphi[:, 4, dm] = (4 * lam[:, 2] - 1) * dlam[:, 2, dm]
+        dphi[:, 5, dm] = 4 * (dlam[:, 2, dm] * lam[:, 0] + lam[:, 2] * dlam[:, 0, dm])
+
+    return phi, dphi
 
 def basis_triangle_P2plus(xr):
-    raise NotImplementedError
+    ni, _ = xr.shape
+    nn = 7
+    phi = np.zeros((ni, nn))
+    dphi = np.zeros((ni, nn, 2))
+    dbubble = np.zeros((ni, 1, 2))
+
+    lam, dlam = barycentric(xr)
+
+    bubble = lam[:, 0] * lam[:, 1] * lam[:, 2]
+
+    phi[:, 0] = lam[:, 0] * (2 * lam[:, 0] - 1) + 3 * bubble
+    phi[:, 1] = 4 * lam[:, 0] * lam[:, 1] - 12 * bubble
+    phi[:, 2] = lam[:, 1] * (2 * lam[:, 1] - 1) + 3 * bubble
+    phi[:, 3] = 4 * lam[:, 1] * lam[:, 2] - 12 * bubble
+    phi[:, 4] = lam[:, 2] * (2 * lam[:, 2] - 1) + 3 * bubble
+    phi[:, 5] = 4 * lam[:, 2] * lam[:, 0] - 12 * bubble
+    phi[:, 6] = 27 * bubble
+
+    for dm in range(2):
+        dbubble[:, 0, dm] = (dlam[:, 0, dm] * lam[:, 1] * lam[:, 2] +
+                             lam[:, 0] * dlam[:, 1, dm] * lam[:, 2] +
+                             lam[:, 0] * lam[:, 1] * dlam[:, 2, dm])
+
+        dphi[:, 0, dm] = (4 * lam[:, 0] - 1) * dlam[:, 0, dm] + 3 * dbubble[:, 0, dm]
+        dphi[:, 1, dm] = 4 * (dlam[:, 0, dm] * lam[:, 1] + lam[:, 0] * dlam[:, 1, dm]) - 12 * dbubble[:, 0, dm]
+        dphi[:, 2, dm] = (4 * lam[:, 1] - 1) * dlam[:, 1, dm] + 3 * dbubble[:, 0, dm]
+        dphi[:, 3, dm] = 4 * (dlam[:, 1, dm] * lam[:, 2] + lam[:, 1] * dlam[:, 2, dm]) - 12 * dbubble[:, 0, dm]
+        dphi[:, 4, dm] = (4 * lam[:, 2] - 1) * dlam[:, 2, dm] + 3 * dbubble[:, 0, dm]
+        dphi[:, 5, dm] = 4 * (dlam[:, 2, dm] * lam[:, 0] + lam[:, 2] * dlam[:, 0, dm]) - 12 * dbubble[:, 0, dm]
+
+    dphi[:, 6, :] = 27 * dbubble[:, 0, :]
+
+    return phi, dphi
 
 def basis_line_P0(xr):
     ni = xr.shape[0]
@@ -221,3 +319,53 @@ def basis_quad_Q2(xr):
             dphi[:, p[i, j], 1] = phi1[:, i] * dphi2[:, j]
 
     return phi, dphi
+
+def barycentric(xr):
+    """
+    lambda(i,j), with i the point in space and j the number
+    derivative of lambda: dlambda(i,j,k), with i the point in space
+    j the number and k the direction in space.
+    Triangles:
+    ---------
+ 
+    the reference region of a triangle is
+       
+       1 |\               eta \in [0,1] 
+       ^ |  \             xi  \in [0,1]
+    eta| |    \           xi + eta <= 1
+         |      \
+       0 ---------
+         0  xi -> 1
+ 
+    The reference coordinates xr=(xi,eta) are in lower triangle of
+    the region [0,1]x[0,1]  (xi+eta<=1)
+ 
+    The three barycentric coordinates are
+ 
+    lambda1=1-xi-eta
+    lambda2=xi
+    lambda3=eta
+    """
+    
+    # Get sizes from xr shape
+    ni, refdim = xr.shape
+    
+    # Initialize the barycentric coordinates and their derivatives
+    nn = 3
+    lambda_ = np.zeros((ni, nn))
+    dlambda = np.zeros((ni, nn, refdim))
+    
+    # Calculate the barycentric coordinates
+    lambda_[:, 0] = 1 - xr[:, 0] - xr[:, 1]
+    lambda_[:, 1] = xr[:, 0]
+    lambda_[:, 2] = xr[:, 1]
+    
+    # Derivatives of the barycentric coordinates
+    dlambda[:, 0, 0] = -1
+    dlambda[:, 0, 1] = -1
+    dlambda[:, 1, 0] = 1
+    dlambda[:, 1, 1] = 0
+    dlambda[:, 2, 0] = 0
+    dlambda[:, 2, 1] = 1
+    
+    return lambda_, dlambda
