acc8464a1a2a206021c46e7a8fda99cfedefe727
Nick Jaensson <n.jaensson@gmail.com>
2023-09-08 22:31:20 +0200

    Update dotest_examples.py

--- dotest_examples.py
+++ dotest_examples.py
@@ -1,3 +1,4 @@
+from math import isclose
 import examples.poisson.poisson1
 import examples.poisson.poisson2
 import examples.poisson.poisson3
@@ -10,15 +11,17 @@ result3 = examples.poisson.poisson3.main()
 result4 = examples.poisson.poisson4.main()
 result5 = examples.stokes.stokes1.main()
 
+eps = 1e-15
+
 print('='*20)
 print('Test result:')
-print(result1==4.071378136849546e-07)
-            #  4.071378150172222e-07 (Matlab)
-print(result2==0.29483065971774913)
-            #  0.294830659717740 (Matlab)
-print(result3==0.14934372115313188)
-            #  0.149343721153131 (Matlab)
-print(result4==6.522070278291991e-07)
-            #  6.522070279402215e-07 (Matlab)
-print(result5==25.865650243176205)
-            #  25.865650243176205 (Matlab)
+print(isclose(result1, 4.071378136849546e-07, abs_tol=eps))
+                       # 4.071378150172222e-07 (Matlab)
+print(isclose(result2, 0.29483065971774913, abs_tol=eps))
+                     # 0.294830659717740 (Matlab)
+print(isclose(result3, 0.14934372115313188, abs_tol=eps))
+                     # 0.149343721153131 (Matlab)
+print(isclose(result4, 6.522070278291991e-07, abs_tol=eps))
+                     # 6.522070279402215e-07 (Matlab)
+print(isclose(result5, 25.865650243176205, abs_tol=eps))
+                     # 25.865650243176205 (Matlab)
