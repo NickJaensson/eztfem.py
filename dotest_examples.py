9f410637e1ac471c1958bcdfbd0fd8699cb51598
Nick Jaensson <n.jaensson@gmail.com>
2023-09-17 13:14:03 +0200

    Add stokes2 to test

--- dotest_examples.py
+++ dotest_examples.py
@@ -4,15 +4,22 @@ import examples.poisson.poisson2
 import examples.poisson.poisson3
 import examples.poisson.poisson4
 import examples.stokes.stokes1
+import examples.stokes.stokes2
 import examples.stokes.streamfunction1
 
+# tests for poisson
 result1 = examples.poisson.poisson1.main()
 result2 = examples.poisson.poisson2.main()
 result3 = examples.poisson.poisson3.main()
 result4 = examples.poisson.poisson4.main()
+
+# tests for stokes
 result5, _, _, _ = examples.stokes.stokes1.main()
+result6, _, _, _ = examples.stokes.stokes2.main()
+
+# test for streamfunction
 _, mesh, problem, u = examples.stokes.stokes1.main()
-result6 = examples.stokes.streamfunction1.main(mesh,problem,u)
+result7 = examples.stokes.streamfunction1.main(mesh,problem,u)
 
 eps = 1e-15
 
@@ -28,5 +35,7 @@ print(isclose(result4, 6.522070278291991e-07, abs_tol=eps))
                      # 6.522070279402215e-07 (Matlab)
 print(isclose(result5, 25.865650243176205, abs_tol=eps))
                      # 25.865650243176205 (Matlab)
-print(isclose(result6, 0.008522786557203416, abs_tol=eps))
+print(isclose(result6, 0.0833333333333339, abs_tol=eps))
+                     # 0.083333333333333 (Matlab)    
+print(isclose(result7, 0.008522786557203416, abs_tol=eps))
                      # 0.008522786557203416 (Matlab)                   
