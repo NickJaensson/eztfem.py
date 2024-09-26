77b8ae919a3ab7419cfce8a16526add71d3c5dcf
NickJaensson <n.jaensson@gmail.com>
2024-09-26 22:14:35 +0200

    Update integrate_boundary_elements.py

--- eztfem/src/integrate_boundary_elements.py
+++ eztfem/src/integrate_boundary_elements.py
@@ -3,10 +3,49 @@ from .pos_array import pos_array
 from .pos_array_vec import pos_array_vec
 
 def integrate_boundary_elements(mesh, problem, element, user, **kwargs):
+    """
+    Integrate boundary elements.
+
+    Parameters
+    ----------
+    mesh : Mesh
+        Mesh object.
+    problem : Problem
+        Problem object.
+    element : function
+        Function handle to the element function routine.
+    user : any
+        Can be used by the user for transferring data to the element routine.
+    **kwargs : optional
+        Additional options.
+        - 'curve' : int
+            Build on given curve number.
+        - 'order' : {'ND', 'DN'}, default='DN'
+            The sequence order of the degrees of freedom on element level:
+            - 'ND' : the most inner loop is over the degrees of freedom.
+            - 'DN' : the most inner loop is over the nodal points.
+            NOTE: The outside loop is always given by the physical quantities.
+        - 'posvectors' : bool, default=False
+            Supply the position of vectors to the element routine.
+
+    Returns
+    -------
+    resultsum : float
+        The sum over all elements of the integral on elements.
+
+    Notes
+    -----
+    Currently, 'curve' is the only possibility for now.
+
+    Examples
+    --------
+    >>> resultsum = integrate_boundary_elements(mesh, problem, element, user)
+    """
+
     # Optional arguments
     curve = kwargs.get('curve', 0)
     order = kwargs.get('order', 'DN')
-    posvectors = kwargs.get('posvectors', 0)
+    posvectors = kwargs.get('posvectors', False)
 
     if curve == 0:
         raise ValueError('Argument curve is missing.')
