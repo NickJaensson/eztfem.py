0264b65d73fd584cc034b9aec9ac81734cf9e79e
NickJaensson <n.jaensson@gmail.com>
2024-09-29 22:20:05 +0200

    Implement flake8 suggestions

--- eztfem/addons/poisson/poisson_elem.py
+++ eztfem/addons/poisson/poisson_elem.py
@@ -1,24 +1,39 @@
 import numpy as np
 from ...core.isoparametric_deformation import isoparametric_deformation
 
+
 def poisson_elem(elem, coor, user, pos):
     """
     Element routine for the Poisson equation: - alpha nabla^2 u = f
-    
-    Parameters:
-    - elem: element number
-    - coor: coordinates of the nodes of the element
-    - user: object containing user data
-    - pos: list of the positions of the degrees of freedom of each physq
 
-    Returns:
-    - elemmat: element matrix
-    - elemvec: element vector
+    Parameters
+    ----------
+    elem : int
+        Element number.
+    coor : ndarray
+        Coordinates of the nodes of the element, shape (n_points, n_dim).
+    user : User
+        User object containing shape function, Gauss points, parameters
+    pos : list of ndarray
+        Positions of the degrees of freedom of each physical quantity.
+
+    Returns
+    -------
+    elemmat : ndarray
+        Element matrix, shape (n_df, n_df).
+    elemvec : ndarray
+        Element vector, shape (n_df,).
+
+    Notes
+    -----
+    This function computes the element matrix and vector for the Poisson
+    equation using isoparametric finite element methods.
+
     """
-    
+
     ninti = user.phi.shape[0]  # number of integration points
     ndf = user.phi.shape[1]    # number of degrees of freedom
-    ndim = coor.shape[1]          # dimension of space
+    ndim = coor.shape[1]       # dimension of space
 
     # Compute mapping of reference to real element
     F, Finv, detF = isoparametric_deformation(coor, user.dphi)
@@ -35,7 +50,8 @@ def poisson_elem(elem, coor, user, pos):
     ndr = Finv.shape[1]
 
     for ip in range(ninti):
-        dphidx[ip] = np.reshape(user.dphi[ip], (ndf, ndr)) @ np.reshape(Finv[ip], (ndr, ndim))
+        dphidx[ip] = np.reshape(user.dphi[ip],
+                                (ndf, ndr)) @ np.reshape(Finv[ip], (ndr, ndim))
 
     # Compute element matrix
     elemmat = np.zeros((ndf, ndf))
@@ -52,13 +68,13 @@ def poisson_elem(elem, coor, user, pos):
 
     # Compute element vector
     elemvec = np.zeros(ndf)
-    
+
     if user.funcnr > 0:
         fg = np.zeros(ninti)
-        
+
         for ip in range(ninti):
             fg[ip] = user.func(user.funcnr, xg[ip])
-        
+
         for i in range(ndf):
             elemvec[i] = np.sum(fg * user.phi[:, i] * detF * user.wg)
 
--- eztfem/addons/poisson/poisson_natboun_curve.py
+++ eztfem/addons/poisson/poisson_natboun_curve.py
@@ -1,33 +1,41 @@
 import numpy as np
-from ...core.isoparametric_deformation_curve import isoparametric_deformation_curve
+from ...core.isoparametric_deformation_curve import \
+    isoparametric_deformation_curve
+
 
 def poisson_natboun_curve(elem, coor, user, pos):
     """
-    Boundary element for a natural boundary on a curve for the Poisson/diffusion equation:
-    - alpha * dudn = h
+    Boundary element for a natural boundary on a curve for the
+    Poisson/diffusion equation: alpha * dudn = h
 
-    Parameters:
-        elem (int): Element number.
-        coor (np.ndarray): Coordinates of the nodes of the element.
-                           Shape: (number of points in space, direction in space).
-        user (dict): Dictionary containing user data.
-            user.phi: Basis functions.
-            user.dphi: Derivatives of basis functions.
-            user.wg: Weights of the integration points.
-            user.coorsys: Coordinate system (0 for Cartesian, 1 for axisymmetric).
-            user.funcnr: Function number for the right-hand side h (flux).
-            user.func: Function for the right-hand side.
-                          Callable with signature: func(funcnr, x)
-        pos (list): List of positions of degrees of freedom of each physq.
+    Parameters
+    ----------
+    elem : int
+        Element number.
+    coor : ndarray
+        Coordinates of the nodes of the element, shape (n_points, n_dim).
+    user : User
+        User object containing shape function, Gauss points, parameters
+    pos : list of ndarray
+        Positions of the degrees of freedom of each physical quantity.
 
-    Returns:
-        elemvec (np.ndarray): The element vector.
+    Returns
+    -------
+    elemvec : ndarray
+        The element vector.
+
+    Notes
+    -----
+    This function computes the element vector for a natural boundary on a curve
+    for the Poisson/diffusion equation using isoparametric finite element
+    methods.
+
     """
 
     # Set some values
     ninti = user.phi.shape[0]  # Number of integration points
-    ndf = user.phi.shape[1]  # Number of degrees of freedom
-    ndim = coor.shape[1]  # Dimension of space
+    ndf = user.phi.shape[1]    # Number of degrees of freedom
+    ndim = coor.shape[1]       # Dimension of space
 
     # Compute mapping of reference to real element
     dxdxi, curvel, normal = isoparametric_deformation_curve(coor, user.dphi)
@@ -46,6 +54,6 @@ def poisson_natboun_curve(elem, coor, user, pos):
         fg = np.zeros(ninti)
         for ip in range(ninti):
             fg[ip] = user.func(user.funcnr, xg[ip])
-        elemvec = -user.phi.T @ ( fg * curvel * user.wg )
+        elemvec = -user.phi.T @ (fg * curvel * user.wg)
 
     return elemvec
