38e99d63d1aa0bf0901be7e2e19f224bea3bec5c
Nick Jaensson <n.jaensson@gmail.com>
2025-10-05 12:09:31 +0200

    Update test_examples.py

--- test/test_examples.py
+++ test/test_examples.py
@@ -19,43 +19,43 @@ eps = 1e-15  # maximal difference for comparing floats
 
 class TestPytfem(unittest.TestCase):
 
-    # value from Matlab: 4.071378150172222e-07
+    # value from eztfem.m (Matlab): 4.071378150172222e-07
     def test_poisson1(self):
         result = poisson1.main()
         self.assertTrue(isclose(result, 4.071378136849546e-07, abs_tol=eps),
                         'poisson1 failed test!')
 
-    # value from Matlab: 0.294830659717740
+    # value from eztfem.m (Matlab): 0.294830659717740
     def test_poisson2(self):
         result = poisson2.main()
         self.assertTrue(isclose(result,  0.29483065971774913, abs_tol=eps),
                         'poisson2 failed test!')
 
-    # value from Matlab: 0.149343721153131 
+    # value from eztfem.m (Matlab): 0.149343721153131 
     def test_poisson3(self):
         result = poisson3.main()
         self.assertTrue(isclose(result, 0.14934372115313188, abs_tol=eps),
                         'poisson3 failed test!')
 
-    # value from Matlab: 6.522070279402215e-07
+    # value from eztfem.m (Matlab): 6.522070279402215e-07
     def test_poisson4(self):
         result = poisson4.main()
         self.assertTrue(isclose(result, 6.522070278291991e-07, abs_tol=eps),
                         'poisson4 failed test!')
 
-    # value from Matlab: 25.865650243176205
+    # value from eztfem.m (Matlab): 25.865650243176205
     def test_stokes1(self):
         result, _, _, _ = stokes1.main()
         self.assertTrue(isclose(result, 25.865650243176205, abs_tol=eps),
                         'stokes1 failed test!')
 
-    # value from Matlab: 0.083333333333333
+    # value from eztfem.m (Matlab): 0.083333333333333
     def test_stokes2(self):
         result, _, _, _ = stokes2.main()
         self.assertTrue(isclose(result, 0.0833333333333339, abs_tol=eps),
                         'stokes2 failed test!')
 
-    # value from Matlab: 0.008522786557203416
+    # value from eztfem.m (Matlab): 0.008522786557203416
     def test_streamfunction1(self):
         _, mesh, problem, u = stokes1.main()
         result = streamfunction1.main(mesh, problem, u)
