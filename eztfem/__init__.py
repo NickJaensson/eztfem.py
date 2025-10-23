1287229891980aca07509721002508a1f361b6d3
Chromosomologist <chromosomologist@gmail.com>
2025-10-23 12:38:00 +0200

    Add explicit exports to package __init__.py

--- eztfem/__init__.py
+++ eztfem/__init__.py
@@ -1,3 +1,21 @@
+__all__ = ("basis_function", "refcoor_nodal_points",
+           "isoparametric_deformation_curve", "isoparametric_deformation",
+           "build_system", "add_boundary_elements", "apply_essential",
+           "Vector", "deriv_vector", "fill_system_vector", "gauss_legendre",
+           "integrate_boundary_elements", "mesh_merge", "pos_array",
+           "pos_array_vec", "Problem", "define_essential", "quadrilateral2d",
+           "distribute_elements", "line1d", "Mesh", "Geometry", "User",
+           "l_shape2d", "two_blocks2d", "poisson_deriv", "poisson_elem",
+           "poisson_natboun_curve", "stokes_elem", "stokes_deriv",
+           "stokes_flowrate_curve", "stokes_natboun_curve", "stokes_pressure",
+           "streamfunction_elem", "streamfunction_natboun_curve",
+           "plot_mesh_pv", "plot_sol", "plot_mesh", "plot_curves",
+           "plot_sol_over_line", "plot_quiver", "plot_vector",
+           "plot_vector_contours", "plot_sol_contour", "plot_vector_over_line",
+           "plot_basis_function", "plot_points_curves", "plot_gauss_legendre",
+           "generate_pyvista_mesh")
+
+
 from .core.shapefunc import basis_function, refcoor_nodal_points, \
     isoparametric_deformation_curve, isoparametric_deformation
 from .core.system_matrix import build_system, add_boundary_elements, \
