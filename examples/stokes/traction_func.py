3aa2b8cc89a1203b9bbe99b57cc9e786fcfd4e35
Nick Jaensson <n.jaensson@gmail.com>
2025-12-29 19:10:59 +0100

    Update examples for pylint/flake8 compliance

--- examples/stokes/traction_func.py
+++ examples/stokes/traction_func.py
@@ -1,14 +1,35 @@
+'''
+Traction functions for the Stokes example problems.
+'''
 import numpy as np
 
 
 def traction_func(nr, x):
+    """
+    Evaluate a vector function as a function of spatial coordinates.
 
-    y = np.zeros(2)
+    Parameters
+    ----------
+    nr : int
+        Function selector.
+    x : numpy.ndarray of shape (ndim,)
+        Spatial coordinates, where ndim is the spatial dimension.
+
+    Returns
+    -------
+    value : numpy.ndarray of shape (2,)
+        Vector function value at x.
+    """
+
+    value = np.zeros(2)
 
     if nr == 1:
-        y[0] = 1.0
-        y[1] = 0.0
+        value[0] = 1.0
+        value[1] = 0.0
+    elif nr == 2:
+        value[0] = 0.0
+        value[1] = x[1]
     else:
         raise ValueError("Invalid value for 'nr'")
 
-    return y
+    return value
