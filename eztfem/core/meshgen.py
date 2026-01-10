f14c412e9d6c851fe4fbf10499b3b27dca4bb959
sharp-eyes <chromosomologist@gmail.com>
2026-01-10 15:42:35 +0100

    feat: add typing to rectangle2d_quad9 function

--- eztfem/core/meshgen.py
+++ eztfem/core/meshgen.py
@@ -1645,7 +1645,8 @@ def rectangle2d_quad5(num_el: typing.Iterable[int], ratio: list[Ratio] | None,
     return mesh
 
 
-def rectangle2d_quad9(num_el, ratio, factor):
+def rectangle2d_quad9(num_el: typing.Iterable[int], ratio: list[Ratio] | None,
+                      factor: list[float]):
     """
     Generate a mesh on region [0,1]x[0,1] using quad elements with 9 nodes.
 
